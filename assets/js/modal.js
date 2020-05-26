6cca4e900aa2263942e2767b5b5480be48afa071
athenac7842 <55065212+athenac7842@users.noreply.github.com>
2020-05-26 11:46:33 -0400

    testing web still

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,8 +1,11 @@
-$("#contacte").submit(function(e) {
-  e.preventDefault();
+// $("#contacte").submit(function(e) {
+//   e.preventDefault();
 
-  var $form = $(this);
-  $.post($form.attr("action"), $form.serialize()).then(function() {
-    alert("Your message has been received!");
-  });
-});
+//   var $form = $(this);
+//   $.post($form.attr("action"), $form.serialize()).then(function() {
+//     alert("Your message has been received!");
+//   });
+// });
+// function show_confirm(){
+//   alert("We are now sending your message. Thank you for reaching out!")
+// }
