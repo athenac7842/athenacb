7f37b5c90e03df7fa8d91ac2de8de6f7e868768e
athenac7842 <55065212+athenac7842@users.noreply.github.com>
2020-05-26 01:37:37 -0400

    netlify alert for submit

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,29 +1,8 @@
-$('#myForm').on('submit', function(e){
-    $('#myModal').modal('show');
-    e.preventDefault();
+$("#confirm").submit(function(e) {
+  e.preventDefault();
+
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    alert("Your message has been received!");
   });
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("confirm");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
+});
