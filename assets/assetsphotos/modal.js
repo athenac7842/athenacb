03b60baaa6c22ba0c39f2cb4f0fe3341a08ff627
athenac7842 <55065212+athenac7842@users.noreply.github.com>
2020-05-28 14:35:35 -0400

    adding modals slowly

--- assets/assetsphotos/modal.js
+++ assets/assetsphotos/modal.js
@@ -1,11 +1,20 @@
-// $("#contacte").submit(function(e) {
-//   e.preventDefault();
+// Get the modal
+var modal = document.getElementById("myModal");
 
-//   var $form = $(this);
-//   $.post($form.attr("action"), $form.serialize()).then(function() {
-//     alert("Your message has been received!");
-//   });
-// });
-// function show_confirm(){
-//   alert("We are now sending your message. Thank you for reaching out!")
-// }
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myimage");
+var modalImg = document.getElementById("image01");
+// var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  // captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closebutton")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
